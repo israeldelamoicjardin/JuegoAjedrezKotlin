b363055a847699193e6c7e239297e642cd14d634
titan\israel <blas@irakasle.eus>
2024-11-17 17:30:56 +0100

    FIX CoreKTX downgrade

--- app/src/main/java/es/israeldelamo/juegocaballos/MainActivity.kt
+++ app/src/main/java/es/israeldelamo/juegocaballos/MainActivity.kt
@@ -13,35 +13,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import es.israeldelamo.juegocaballos.ui.theme.JuegoCaballosTheme
 
+
+/**
+ * Punto de entrada de la aplicación.
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            JuegoCaballosTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JuegoCaballosTheme {
-        Greeting("Android")
-    }
-}
